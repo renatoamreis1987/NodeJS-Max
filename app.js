bea52f11bca29457c67ebbc817d76c28f0860732
Renato Reis <email@renatoamreis.com>
2021-03-06 14:17:34 +0100

    Adding files, starting with Express.js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const http = require("http");
+const express = require("express");
 
-const routes = require('./routes')
+const app = express();
 
-const server = http.createServer(routes);
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 
-server.listen(3000);
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
+
+app.listen(3000);
