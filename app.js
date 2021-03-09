6565308813a88dc238bbae49cd3bf80fcddd6be8
Renato Reis <email@renatoamreis.com>
2021-03-09 18:54:45 +0100

    Adding files from Section 6: Working with Dynamic Content & Adding Templating Engines

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require("express"); // Express.js
 
 const app = express(); // Use Express
 
+app.set('view engine', 'ejs') // The view engine
+app.set('views', 'views') // This line not needed as by default is views, but in case is another folder
+
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
@@ -14,7 +17,8 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", {docTitle: 'Page Not Found¡' })
 });
 
 app.listen(3000);
